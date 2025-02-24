6c8c32dd0374590ed0cadba32ac3ec643674cfb2
LeGush <alekseevgordey@gmail.com>
2025-02-24 17:13:58 +0300

    Дополнительное задание

--- laba-7.py
+++ laba-7.py
@@ -2,8 +2,11 @@ import requests
 import webbrowser
 import re
 import sys
+from tkinter import Tk, Button, Label
+from PIL import Image, ImageTk
+from io import BytesIO
 
-# # первое задание
+# первое задание
 api_key_weather = '6a44263814d48cbe2101ccda16e0f365'
 city_name = 'Kutná Hora'
 url_weather = f'https://api.openweathermap.org/data/2.5/weather?' \
@@ -33,4 +36,30 @@ webbrowser.open_new(f'https://media.merriam-webster.com/'
                     f'audio/prons/en/us/mp3/{word[0]}/'
                     f'{data_dict["hwi"]["prs"][0]["sound"]["audio"]}.mp3')
 print(f"""Краткое определение: {re.sub(r'{.*?}', '', data_dict["shortdef"][0])}
-Этимология: {re.sub(r'{.*?}', '', data_dict["et"][0][1])}""")
+Этимология: {re.sub(r'{.*?}', '', data_dict['et'][0][1])}""")
+
+# доп задание
+api_nasa = 'z2QfXds3G5wxMPAGXoikUVxh4zqg7b8DWdhhUSoH'
+url_nasa = f'https://api.nasa.gov/planetary/apod?api_key={api_nasa}'
+data_nasa = requests.get(url_nasa).json()
+image_nasa = data_nasa['hdurl']
+
+
+def show_pict():
+        pict = Image.open(BytesIO(requests.get(image_nasa).content))
+        width, height = pict.size
+        new_width = 720
+        new_height = int(height * (new_width / width))
+        pict = pict.resize((new_width, new_height),
+                             Image.Resampling.LANCZOS)
+        pict = ImageTk.PhotoImage(pict)
+        show_pict.image_label = Label(root, image=pict)
+        show_pict.image_label.image = pict
+        show_pict.image_label.pack()
+
+
+root = Tk()
+root.title('Изображение дня')
+btn = Button(root, text='Загрузить изображение', command=show_pict)
+btn.pack(pady=20)
+root.mainloop()
